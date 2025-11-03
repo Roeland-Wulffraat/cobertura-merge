4d60398fef495a8cf08f4b5c70826858bcbf690b
Roeland Wulffraat <roeland.wulffraat@pggm.nl>
2025-11-03 15:07:33 +0100

    deprecated util APIs

--- src/input.ts
+++ src/input.ts
@@ -37,7 +37,7 @@ export function validateArgs(args: ParsedArgs): void {
     process.exit(1);
   }
 
-  if (args._.length < 3 || args.o === true || isArray(args.o) || isString(args.p) || isArray(args.p)) {
+  if (args._.length < 3 || args.o === true || Array.isArray(args.o) || typeof args.p === "string" || Array.isArray(args.p)) {
     // Input error
     printHelp();
   }
